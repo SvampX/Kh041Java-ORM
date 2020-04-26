ecc8979cca7082f7f2889ad5cb746a058f92718c
Alexey <alzozp@gmail.com>
2020-04-26 14:50:10 +0300

    added pool

--- src/main/java/connections/ConnectionToDB.java
+++ src/main/java/connections/ConnectionToDB.java
@@ -5,12 +5,23 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 
-public class ConnectionToDB  {
+public class ConnectionToDB {
 
-    public Connection getConnection() throws SQLException {
+    private List<Connection> availableConList = new ArrayList<>();
+    private List<Connection> unvailableConList = new ArrayList<>();
+    private int defaultConNumber = 10;
+
+    public ConnectionToDB() throws SQLException {
+        for (int i = 0; i < defaultConNumber; i++)
+            availableConList.add(createConnection());
+    }
+
+    private Connection createConnection() throws SQLException {
         InputStream input = null;
         try {
             input = new FileInputStream("src/main/resources/config.properties");
@@ -26,7 +37,6 @@ public class ConnectionToDB  {
             e.printStackTrace();
         }
 
-        // get the property value and print it out
         System.out.println(prop.getProperty("db.url"));
         System.out.println(prop.getProperty("db.user"));
         System.out.println(prop.getProperty("db.password"));
@@ -37,18 +47,20 @@ public class ConnectionToDB  {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-
         return DriverManager.getConnection(prop.getProperty("db.url"), prop.getProperty("db.user"), prop.getProperty("db.password"));
-
     }
 
+    public Connection getConnection() {
+        Connection con = availableConList.get(0);
+        unvailableConList.add(con);
+        availableConList.remove(con);
+        return con;
+    }
 
-
-
-
-
-
-
+    public void releaseConnection(Connection con) {
+        availableConList.add(con);
+        unvailableConList.remove(con);
+    }
 }
 
 
