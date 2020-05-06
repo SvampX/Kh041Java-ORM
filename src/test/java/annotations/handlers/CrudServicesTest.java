85537dc04ea077facf47630763d280310fbc982f
Mikle Savchenko <svampx@gmail.com>
2020-05-06 18:19:52 +0300

    pre merging commit, fetching foreign keys altering updates

--- src/test/java/annotations/handlers/CrudServicesTest.java
+++ src/test/java/annotations/handlers/CrudServicesTest.java
@@ -1,18 +1,24 @@
 package crud;
 
-import annotations.handlers.TablesInitializationTest;
+import annotations.handlers.DBTable;
+import annotations.handlers.EntityToTableMapper;
+import annotations.handlers.configuration.Phone;
 import annotations.handlers.configuration.SimpleEntity;
 import connections.ConnectionToDB;
+import crud.CrudServices;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class CrudServicesTest {
+
     Connection connection;
     CrudServices crudServices;
-
     @BeforeEach
     void setUp() {
         try {
@@ -26,15 +32,21 @@ class CrudServicesTest {
     }
 
     @Test
-    void readEntityById() {
+    void initTables() {
+        crudServices.initTables(connection);
+    }
 
+    @Test
+    void readEntityById() {
+        Set<DBTable> tables = EntityToTableMapper.getTables();
         SimpleEntity simpleEntity = (SimpleEntity) crudServices.readById( 1, SimpleEntity.class);
         System.out.println("simpleEntity = " + simpleEntity);
     }
 
     @Test
     void createEntityTest() {
-        TablesInitializationTest tablesInitializationTest = new TablesInitializationTest();
-        tablesInitializationTest.tableCreationTest();
+        Phone phone = new Phone();
+        phone.setNumber("937-99-92");
+        crudServices.create(phone);
     }
 }
